1efc7c70191ca7f7b41f19f4e2daa644d8ea45ef
q92helea <q92helea@hotmail.com>
2023-12-05 12:52:23 +0100

    script1

---
+++ ejemplo.ps1
@@ -0,0 +1,14 @@
+#variables de tipo booleano
+[bool]$ hayararchivos=$false
+write-host "Tenemos más archivos? $hayararchivos"
+
+#variables de tipo fecha
+[datetime] $fechanacimiento ="10/9/2020 21:15:48"
+write-host "El bebe nació el $fechaNacimiento"
+
+#Ahora vamos acceder campos de tipo datetime
+
+write-host "El mes de nacimiento es $($fechaNacimiento.Month"
+
+write-host "El mes de nacimiento es $($fechaNacimiento.Day)"
+write-host "El mes de nacimiento es $($fechaNacimiento.Year)"
