249e1cd679f00a0b3ab7f31e77fd46e47534d9aa
AsirH <45197222+asirheras@users.noreply.github.com>
2025-05-28 23:34:49 +0200

    C_L2EC2.ps1

---
+++ vagoredomado/scripts/L2EC2.ps1
@@ -0,0 +1,16 @@
+﻿#script: LEC2.ps1
+#fecha: 20250528
+#Autor: AsirHeras
+
+<#Descripción: El script manda un archivo desde l máquina local a 
+la máquina remota (EC2)
+#>
+$keyPath    = "$HOME\.ssh\llave.pem"
+$remoteUser = "ubuntu"
+$remoteHost = "ec2-xx-xx-xx-xx.compute-1.amazonaws.com"
+$remoteTmp  = "/home/$remoteUser"
+$localDir   = "C:\Scripts"
+$fileName   = "files.rar"
+
+# Copia local a remoto
+scp -i $keyPath "$localDir\\$fileName" "$remoteUser@$remoteHost:$remoteTmp/"
